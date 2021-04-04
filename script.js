fb067d340835d34963ccfc71dec70f3f9f3e3f39
Animesh Jaiswal <animeshgkp15@gmail.com>
2021-04-05 00:44:38 +0530

    editing

--- script.js
+++ script.js
@@ -5,6 +5,32 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    $(".scroll-up-btn").click(function(){
+        $("html, body").animate({scrollTop: 0});
+    });
+
+    var typed = new Typed(".typing", {
+        strings: ["Programmer","Developer" ,"Freelancer", "CTF-Player","Bug-Hunter"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+
+    });
+
+    var typed = new Typed(".typing2", {
+        strings: ["Programmer","Developer" ,"Freelancer", "CTF-Player","Bug-Hunter"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+
     });
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
